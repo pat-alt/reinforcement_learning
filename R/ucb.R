093fe77b9de973d2a516507a958c23f6a58e2974
pat-alt <altmeyerpat@gmail.com>
2021-05-08 09:04:59 +0200

    ucb now seems to check out

--- R/ucb.R
+++ R/ucb.R
@@ -1,47 +1,57 @@
 ucb <- function(
   mab,
-  upper_confidence_bounds = NULL
+  action_values = NULL
 ) {
 
-  # Initialization: ----
-  unpack(mab) # unpack variables
-  if (is.null(upper_confidence_bounds)) {
-    upper_confidence_bounds <- rep(0,K) # initial upper confidence bounds
+  # Parameters: ----
+  uncertainty <- function(action_value,m,delta) {
+    ifelse(m==0, Inf, sqrt((2 * action_value * log(1/delta)) / m) + (2*log(1/delta)) / m) # uncertainty
   }
+
+  # Initialization: ----
+  unpack(mab) # unpack bandit problem
+  delta <- sqrt(1/T_)
   policy <- rep(0, horizon) # vector to keep track of choices
   times_chosen <- rep(0,K) # count of times that actions were chosen
   T_ <- 1 # iteration counter
+  if (is.null(action_values)) {
+    action_values <- rep(0,K)
+  }
+  ucb <- action_values + uncertainty(action_values, times_chosen, delta) # initial upper confidence bounds
 
   # Recursion: ----
   while (T_ <= horizon) {
-    delta <- sqrt(1/T_)
+
     # Select arm:
-    if (sum(upper_confidence_bounds==max(upper_confidence_bounds))==1) {
-      arm <- which.max(upper_confidence_bounds) # select maximal value
+    if (sum(ucb==max(ucb))==1) {
+      arm <- which.max(ucb) # select maximal value
     } else {
       # should there be no maximum, draw uniformly:
-      arm <- sample(which(upper_confidence_bounds==max(upper_confidence_bounds)),1)
+      arm <- sample(which(ucb==max(ucb)),1)
     }
     policy[T_] <- arm # update policy
     times_chosen[arm] <- times_chosen[arm] + 1 # update counter
+
     # Observe reward:
     r <- rewards[T_,get(colnames(rewards)[arm])]
-    # Update upper confidence bound:
-    q <- upper_confidence_bounds[arm]
-    m <- times_chosen[arm]
-    upper_confidence_bounds[arm] <- q +
-      1/m * (r - q) + # action value estimate
-      sqrt((2/m * log(1/delta)) / m) + (2*log(1/delta)) # uncertainty
+
+    # Update parameters and estimates for T+1:
     T_ <- T_ + 1
+    delta <- sqrt(1/T_)
+    q <- action_values[arm] # action value at T
+    m <- times_chosen[arm] # times chose at T
+    action_values[arm] <- q + 1/m * (r - q) # action value increment
+    ucb[arm] <- action_values[arm] + uncertainty(action_values[arm], m, delta) # UCB increment
   }
 
   # Output: ----
   output <- list(
     policy = policy,
     algo = list(
-      upper_confidence_bounds = upper_confidence_bounds,
+      upper_confidence_bounds = ucb,
       times_chosen = times_chosen
-    )
+    ),
+    mab = mab
   )
   class(output) <- "policy"
   return(output)
